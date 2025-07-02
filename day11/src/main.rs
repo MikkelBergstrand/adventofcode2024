e682c16d62deda1e3bc07e816c257288f2a34309
Mikkel Bergstrand <mikkel.bergstrand@gmail.com>
2025-07-02 03:07:18 +0200

    day11b

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,41 +1,70 @@
-use std::fs;
+use std::{collections::HashMap, fs, usize};
 
-struct Cached {
-    value: i32,
-    result: i32,
+#[derive(Eq, Hash, PartialEq, Debug)]
+struct NodeKey {
+    depth: usize,
+    value: usize,
 }
 
-const ITER_NUM: i32 = 75;
+const ITER_NUM: usize = 75;
+
+fn count_node(
+    value: usize,
+    depth: usize,
+    max_depth: usize,
+    cache: &mut HashMap<NodeKey, usize>,
+) -> usize {
+    // Check if cached
+    let key = NodeKey {
+        value,
+        depth: max_depth - depth,
+    };
+    if let Some(cache_ret) = cache.get(&key) {
+        return *cache_ret;
+    }
+
+    // Terminate at leaf nodes of search
+    if depth >= max_depth {
+        return 1;
+    }
+
+    // Compute result according to rules of the game.
+    let result: usize;
+    if value == 0 {
+        result = count_node(1, depth + 1, max_depth, cache);
+    } else if value.to_string().len() % 2 == 0 {
+        let str_num = value.to_string();
+        let num1: usize = str_num[0..str_num.len() / 2].parse::<usize>().unwrap();
+        let num2: usize = str_num[str_num.len() / 2..].parse::<usize>().unwrap();
+        result = count_node(num1, depth + 1, max_depth, cache)
+            + count_node(num2, depth + 1, max_depth, cache);
+    } else {
+        result = count_node(2024 * value, depth + 1, max_depth, cache);
+    }
+
+    // Update cache with new value
+    cache.insert(key, result);
+    return result;
+}
 
 fn main() {
-    let mut nums: Vec<usize> = fs::read_to_string("input.txt")
+    let mut cache: HashMap<NodeKey, usize> = HashMap::new();
+    let nums: Vec<usize> = fs::read_to_string("input.txt")
         .unwrap()
         .replace("\n", "")
         .split(" ")
         .map(|num| num.parse::<usize>().unwrap())
         .collect();
 
-    for iter in 0..6 {
-        let mut i = 0;
-        while i < nums.len() {
-            if nums[i] == 0 {
-                nums[i] = 1;
-                i += 1;
-            } else if nums[i].to_string().len() % 2 == 0 {
-                let str_num = nums[i].to_string();
-                nums[i] = str_num[0..str_num.len() / 2].parse::<usize>().unwrap();
-                nums.insert(
-                    i + 1,
-                    str_num[str_num.len() / 2..].parse::<usize>().unwrap(),
-                );
-                i += 2;
-            } else {
-                nums[i] *= 2024;
-                i += 1;
-            }
+    let mut result = 0;
+    // Use a "iterative deepening" approach. Advance depth of search by 1 each time.
+    // Since the cache is maintained between iterations, speed is supringsly fast!
+    for iter in 1..(ITER_NUM + 1) {
+        result = 0;
+        for num in nums.iter() {
+            result += count_node(*num, 0, iter, &mut cache);
         }
-        dbg!((iter, &nums));
     }
 
-    dbg!(nums.len());
+    dbg!(result);
 }
